d20e5e484e4fc4f0d6c7e2a41c58f2de70ee84da
EuJinnLucaShow <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-03-08 12:04:02 +0000

    Deploying to gh-pages from @ EuJinnLucaShow/goit-js-hw-11@7eaeba5918388312ca477222ddd9fc8fbe609980 🚀

---
+++ index.6e9fef83.js
@@ -0,0 +1,2 @@
+SmoothScroll({animationTime:800,stepSize:75,accelerationDelta:30,accelerationMax:2,keyboardSupport:!0,arrowScroll:50,pulseAlgorithm:!0,pulseScale:4,pulseNormalize:1,touchpadSupport:!0});
+//# sourceMappingURL=index.6e9fef83.js.map
---
+++ index.79a76a80.js
@@ -0,0 +1,2 @@
+SmoothScroll({animationTime:800,stepSize:75,accelerationDelta:30,accelerationMax:2,keyboardSupport:!0,arrowScroll:50,pulseAlgorithm:!0,pulseScale:4,pulseNormalize:1,touchpadSupport:!0});
+//# sourceMappingURL=index.79a76a80.js.map
